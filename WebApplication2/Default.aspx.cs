9665de5691fced5899f15adcdd60aabd71fdf632
dave <padot@umich.edu>
2019-12-18 15:32:55 -0500

    Add project files.

---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var exe = $@"c:\windows\system32\ping.exe";
+            var start = new ProcessStartInfo
+            {
+                FileName = exe,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                Arguments = "google.com"
+            };
+            using var process = Process.Start(start);
+            using var reader = process.StandardOutput;
+            var result = reader.ReadToEnd();
+            Response.Write(result);
+
+        }
+    }
+}
